0e93995710efeb5249759424263922557a9b74a4
Jhonatan Latorre Sierra <61359329+Jhonatanls@users.noreply.github.com>
2024-04-16 14:27:18 -0500

    Prinipio para arreglos del codigo

--- movies/models.py
+++ movies/models.py
@@ -20,15 +20,15 @@ class Indice_madurez(models.Model):
     
 class Pelicula(models.Model):
     titulo = models.CharField(max_length=255)
-    genero = models.ManyToManyField(Genero)
+    genero = models.ManyToManyField(Genero, related_name='generos')
     calificacion = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     cantidad_vistas = models.IntegerField()
     argumento = models.CharField(max_length=255)
-    actor = models.ManyToManyField(Actor)
+    actor = models.ManyToManyField(Actor, related_name='actors')
     duracion = models.IntegerField()
     fecha_publicacion = models.DateField()
-    indice_madurez = models.ManyToManyField(Indice_madurez)
+    indice_madurez = models.ManyToManyField(Indice_madurez, related_name='indices')
     imagen_cartelera = models.URLField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.titulo}({self.calificacion}) - {self.argumento} "
+        return f"{self.titulo} - Actores: {', '.join(g.name for g in self.actor.all())} - Géneros: {', '.join(g.name for g in self.genero.all())}"
